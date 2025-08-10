14f14331b1e27bbcf4d5905a29beb885663fa40a
Trong Khanh <lichessbotvietnam@gmail.com>
2025-08-10 18:18:06 +0700

    new

--- h268sxso.py
+++ h268sxso.py
@@ -1,21 +1,16 @@
-s = input()
-Str = []; Num = []
-tx = s[0]; ty = ""
-for i in range (1,len(s)):
-    if s[i].isdigit():
-        ty = ty + s[i]
-        if tx != "":
-            Str.append(tx); tx = ""
+s = input(); s = "a" + s + "a"
+s1 = s2 = ""
+for c in s:
+    if c.isdigit():
+        s1 = s1 + " "
+        s2 = s2 + c
     else:
-        tx = tx + s[i]
-        if ty != "":
-            Num.append(int(ty)); ty = ""
-if tx != "":
-    Str.append(tx); tx = ""
-Str[0] = Str[0][1:]; Str[-1] = Str[-1][:len(Str[-1])- 1]
+        s1 = s1 + c
+        s2 = s2 + " "
+Str = s1.split()
+Num = list(map(int, s2.split()))
 Num.sort()
-s = ""
-for i in range (len(Num)):
-    s = s + Str[i] + str(Num[i])
-s = s + Str[len(Str)-1]
-print(s)
+res = ""
+for i in range(len(Num)):
+    res += Str[i] + str(Num[i])
+print(res)
--- h270maxsum.py
+++ h270maxsum.py
@@ -1,2 +1,7 @@
 n = int(input())
 a = list(map(int, input().split()))
+Sum = Res = -1e18
+for x in a:
+    Sum = max(Sum + x, x)
+    Res = max(Res, Sum)
+print(Res)
